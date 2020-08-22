5e2bc34cf914226d129193852c2e0bf89efca5e8
kwonyeseong <21200050@handong.edu>
2020-08-22 13:39:24 +0900

    [ADD] application.properties file (show sql)

---
+++ src/main/java/com/yebali/spring_study/springboot/domain/posts/Posts.java
@@ -0,0 +1,34 @@
+package com.yebali.spring_study.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto increase
+    private int id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
