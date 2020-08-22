4654693d45e221f2aec74e07a659a0eb1d823a1f
kwonyeseong <21200050@handong.edu>
2020-08-22 12:51:40 +0900

    [UPDATE] HelloController + TEST

---
+++ src/main/java/com/yebali/spring_study/springboot/Application.java
@@ -0,0 +1,13 @@
+package com.yebali.spring_study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication//:스프링 부트의 자동설정 Bean읽기와 생성을 모두 자동으로 설정 항상 프로젝트 최상단에 있어야함
+public class Application {
+    public static void main(String[] args) {
+        //SpringApplicaion으로 인해 내장 WAS를 실행
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/yebali/spring_study/springboot/web/HelloController.java
@@ -0,0 +1,20 @@
+package com.yebali.spring_study.springboot.web;
+
+import com.yebali.spring_study.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController//:JSON을 반환하는 컨트롤러로 만들어준다.
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+}
