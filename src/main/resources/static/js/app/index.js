ed620a640cd095694477373291e6d9e3b40f1136
yebali <qweas68@naver.com>
2020-08-29 11:06:31 +0900

    [FIX] Revise save bug

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -9,14 +9,14 @@ var main = {
         var data = {
             title : $('#title').val(),
             author : $('#author').val(),
-            content : $('content').val()
+            content : $('#content').val()
         };
 
         $.ajax({
             type: 'POST',
             url: '/api/v1/posts',
             dataType: 'json',
-            contentType: '/application/json; charset=utf-8',
+            contentType: 'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function() {
             alert('글이 등록되었습니다.');
