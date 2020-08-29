eb0e088dd9ff9fabe8885af04209cd9f16d56308
yebali <qweas68@naver.com>
2020-08-29 12:28:12 +0900

    [UPDATE] Posts revise, delete

--- src/main/java/com/yebali/spring_study/springboot/service/posts/PostsService.java
+++ src/main/java/com/yebali/spring_study/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.yebali.spring_study.springboot.service.posts;
 
 import com.yebali.spring_study.springboot.domain.posts.Posts;
 import com.yebali.spring_study.springboot.domain.posts.PostsRepository;
+import com.yebali.spring_study.springboot.web.dto.PostsListResponseDto;
 import com.yebali.spring_study.springboot.web.dto.PostsResponseDto;
 import com.yebali.spring_study.springboot.web.dto.PostsSaveRequestDto;
 import com.yebali.spring_study.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor //postRepository를 @Autowired가 아닌 직접 생성자로 주입받음
 @Service
@@ -37,5 +41,21 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        //Entity인 Posts를 그대로 사용하지 않고 PostsListResponseDto라는 객체를 별도로 지정하여 사용.
+        //Entity자체를 직접 사용하는건 좋지 않기 때문
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        postsRepository.delete(posts);
     }
 }
---
+++ src/main/java/com/yebali/spring_study/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.yebali.spring_study.springboot.web.dto;
+
+import com.yebali.spring_study.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
