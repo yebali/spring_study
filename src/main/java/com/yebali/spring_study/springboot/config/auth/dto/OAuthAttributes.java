7a759f3648478245cb8515b422a5b47b7f97229b
yebali <qweas68@naver.com>
2020-09-03 23:03:17 +0900

    [UPDATE] apply naver login

--- src/main/java/com/yebali/spring_study/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/yebali/spring_study/springboot/config/auth/dto/OAuthAttributes.java
@@ -30,6 +30,10 @@ public class OAuthAttributes {
     public static OAuthAttributes of (String registrationID,
                                       String userNameAttributeName,
                                       Map<String, Object> attributes) {
+        if("naver".equals(registrationID)) {
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -40,6 +44,20 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        System.out.println(attributes);
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
